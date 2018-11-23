e2c5ef2aa3e28e3ab43b8fa041e778026b2358be
hanseonghye <hans12312@naver.com>
2018-11-23 15:08:55 +0900

    add

---
+++ src/main/java/com/example/demo/util/Util.java
@@ -0,0 +1,21 @@
+package com.example.demo.util;
+
+
+
+/**
+ * Util class for constants and generic methods
+ */
+
+public class Util {
+
+	// For the GCM connection
+	public static final String FCM_SERVER = "fcm-xmpp.googleapis.com";
+	public static final int FCM_PORT = 5236;
+	public static final String FCM_ELEMENT_NAME = "gcm";
+	public static final String FCM_NAMESPACE = "google:mobile:data";
+	public static final String FCM_SERVER_CONNECTION = "gcm.googleapis.com";
+	public static final String fcmProjectSenderId = "";
+	public static final String fcmServerKey = "";
+	public static final String toRegId = "";
+
+}
